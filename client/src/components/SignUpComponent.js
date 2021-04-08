c3358ed9bd41a1d7367714c739198169deb2c84b
Juan <jd.berbeo@gmail.com>
2021-04-08 12:43:17 -0500

    delete files

---
+++ client/src/components/SignUpComponent.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import  axios from 'axios'
+
+
+
+export const SignUpComponent = () => {
+
+    const initialState = {email:'', password: ''}
+    const [state, setstate] = useState(initialState)
+    
+    const handleOnchange =(e)=>{
+        const{name,value}=e.target
+        setstate({...state,[name]:value})
+        console.log(state)    
+    }
+
+    const handleOnClick = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/signup', state)
+        .then(function (response) {
+        console.log(response);
+            })
+        .catch(function (error) {
+        console.log(error);
+        });
+
+    }
+    return (
+        <div>
+            <form>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleOnchange} value={state.email} name="email"/>
+                    <div id="emailHelp" className="form-text"> We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" onChange={handleOnchange} value={state.password} name="password"/>
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={handleOnClick}>Submit</button>
+            </form>
+        </div>
+    )
+}
